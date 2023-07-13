a1bb57806348cec631715f8132505559b4eb04aa
skyler <skyler@skylers-MacBook-Pro.local>
2023-07-13 14:46:11 -0300

    update

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,7 @@ function getCoordinates(cityValue) {
     .then(data => {
       console.log(data)
       getCurrentWeather(data[0].lat, data[0].lon)
+      getFiveDayWeather(data[0].lat, data[0].lon)
     })
 }
 
@@ -24,20 +25,14 @@ function getCurrentWeather(lat, lon) {
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      let cityName = $("<h3>").text(data.name)
+      let cityName = $("<h2>").text(data.name)
       $("#currentWeather").append(cityName)
-      var temp = $("<h3>").text("temp: " + data.main.temp)
-      $("#currentWeather").append(temp)
-      let theWeather = $("<h3>").text("weather: " + data.weather[0].description)
-      $("#currentWeather").append(theWeather)
-      const dateToday = $("<h3>").text(dayjs().format("MM / DD / YYYY"))
-      $("#currentWeather").append(dateToday)
-      let humidity = $("<h3>").text("humidity: " + data.main.humidity)
-      $("#currentWeather").append(humidity)
-
-      // icon for tempature
+      localStorage.setItem("city", cityName)
+      //$("#searchHistory").innerText = JSON.parse(localStorage.getItem("city"))
       if (data.weather[0].main == "Clouds") {
-        $("#currentWeather").append("<i class='fa-solid fa-cloud'></i>")
+        $("#currentWeather").append(
+          "<i class='fa-solid fa-cloud fa-5x' style></i>"
+        )
       } else if (data.weather[0].main == "Thunderstorm") {
         $("#currentWeather").append("<i class='fa-solid fa-cloud-bolt'></i>")
       } else if (data.weather[0].main == "clear") {
@@ -51,6 +46,50 @@ function getCurrentWeather(lat, lon) {
       } else if (data.weather[0].main == "Snow") {
         $("currentWeather").append("<i class='fa-solid fa-snowflake'></i>")
       }
+      const dateToday = $("<h4>").text(dayjs().format("MM / DD / YYYY"))
+      $("#currentWeather").append(dateToday)
+      var temp = $("<h4>").text("temp: " + data.main.temp)
+      $("#currentWeather").append(temp)
+      let theWeather = $("<h4>").text("weather: " + data.weather[0].description)
+      $("#currentWeather").append(theWeather)
+      let humidity = $("<h4>").text("humidity: " + data.main.humidity)
+      $("#currentWeather").append(humidity)
+    })
+}
+
+function getFiveDayWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
+  )
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      //for (data.list[6,14,22,30,38]) {
+      //   if (data.weather[0].main == "Clouds") {
+      //     $("#currentWeather").append(
+      //       "<i class='fa-solid fa-cloud fa-5x' style></i>"
+      //     )
+      //   } else if (data.weather[0].main == "Thunderstorm") {
+      //     $("#currentWeather").append("<i class='fa-solid fa-cloud-bolt'></i>")
+      //   } else if (data.weather[0].main == "clear") {
+      //     $("#currentWeather").append("<i class='fa-solid fa-sun'></i>")
+      //   } else if (data.weather[0].main == "Rain") {
+      //     $("#currentWeather").append(
+      //       "<i class='fa-solid fa-cloud-showers-heavy'></i>"
+      //     )
+      //   } else if (data.weather[0].main == "Drizzle") {
+      //     $("#currentWeather").append("<i class='fa-solid fa-cloud-rain'></i>")
+      //   } else if (data.weather[0].main == "Snow") {
+      //     $("currentWeather").append("<i class='fa-solid fa-snowflake'></i>")
+      //   }
+      //   const dateToday = $("<h4>").text(str.split(data.dt_txt))
+      //   $("#currentWeather").append(dateToday)
+      //   var temp = $("<h4>").text("temp: " + data.main.temp)
+      //   $("#currentWeather").append(temp)
+      //   let theWeather = $("<h4>").text("weather: " + data.weather[0].description)
+      //   $("#currentWeather").append(theWeather)
+      //   let humidity = $("<h4>").text("humidity: " + data.main.humidity)
+      //   // $("#currentWeather").append(humidity)
     })
 }
 
